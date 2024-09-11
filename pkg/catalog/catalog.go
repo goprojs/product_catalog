ed0c2fc5230886e019eb1f4811f023e7815cceea
goprojs <ayradas37@gmail.com>
2024-09-11 18:49:23 +0530

    Adding pkg and ui

---
+++ pkg/catalog/catalog.go
@@ -0,0 +1,45 @@
+package catalog
+
+type Cake struct {
+	ID          string  `json:"id"`
+	Title       string  `json:"title"`
+	Description string  `json:"description"`
+	Category    string  `json:"category"`
+	Price       float64 `json:"price"`
+	Weight      float64 `json:"weight"` // Weight in gms
+	Image       string  `json:"image"`  // URL of the image
+}
+
+var cakes = []Cake{
+	{
+		ID:          "1",
+		Title:       "Chocolate Forest",
+		Category:    "Birthday",
+		Description: "Filled with chocolate",
+		Weight:      500,
+		Price:       530.0,
+		Image:       "http://cdn.images.cake1.png",
+	},
+	{
+		ID:          "2",
+		Title:       "Chocolate Vanila",
+		Category:    "Birthday",
+		Description: "Filled with vanila chocolate",
+		Weight:      500,
+		Price:       420.0,
+		Image:       "http://cdn.images.cake2.png",
+	},
+	{
+		ID:          "3",
+		Title:       "Red Velvet",
+		Category:    "Annivesary",
+		Description: "Filled with red velvet",
+		Weight:      500,
+		Price:       600.0,
+		Image:       "http://cdn.images.cake3.png",
+	},
+}
+
+func getCakes(c *gin.Context) []Cake {
+	return cakes
+}
